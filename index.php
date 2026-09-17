6c4e0d47778a3d28478fa28af1d2ea4e920053eb
ryanmurphy36 <148974569+ryanmurphy36@users.noreply.github.com>
2026-09-17 11:51:49 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,142 @@
+<?php $currentPage = "Home";?>
+
+<?php include "assets/inc/header.php"; ?>
+
+        <?php include "assets/inc/nav.php"; ?>
+        <main>
+            <article>
+                <h1> Ryan Murphy </h1>
+                <h2 class="cat-title">Web Designer, UI/UX Designer</h2>
+
+                <img id="ryan" src="assets/images/ryan.png" alt="picture of the sites creator, ryan">
+                <div  class="subacctext" id="about">
+                    <p>I'm a college student at RIT developing skills in web development, user-centered design, graphic design, and more.</p>
+                    <p>I am seeking a UI/UX internship/co-op utilizing these skills. I am available in Summer 2027.</p>
+                    <p>I'm obtaining my Human-Centered Computing BS, which covers human-first design and studies how humans interact with computers. I take courses in web design, graphic design, and psychology! </p>
+                </div>
+            </article>
+
+            <article id="edu">
+                <h2 class="cat-title">Education</h2>
+                <section class="subcontent">
+                <h3>Rochester Institute of Technology</h3>	    
+
+                <div class="subacctext">Expected May 2029</div>
+                <div class="subacctext">Bachelor of Science, Human-Centered Computing</div>
+
+                <h3>Relevant Coursework:</h3>
+
+                <ul>
+                    <li>Foundations of Mobile Design</li>
+                    <li>Client Programming</li>
+                    <li>Prototyping and Usability Testing</li>
+                    <li>Web and Mobile II</li>
+                    <li>New Media Digital Design Survey</li>
+                </section>
+                </ul>
+
+            </article>
+
+			<article id="project">
+                <div class="cat-title">
+                    <h2>Projects</h2>
+                    <p>A hub of links to school projects whether they be websites, prototypes, or graphic design. </p>
+                    <p>Read through the descriptions of my projects and click the links to view them in full. Enjoy! </p>
+                </div>
+				
+                <section id="project-list">
+                    <section class="subcontent">
+                        <h3>Web Design & Development</h3>
+
+                        <a href="projects/world_of_sonic/index.php">The World of Sonic the Hedgehog</a>
+
+                        <p class="subacctext">for ISTE-240 (Web and Mobile II) at RIT</p>
+
+                        <p>A comprehensive information hub into everything relating to the world of the video game series Sonic the Hedgehog. Test your skills with the ten question quiz when you're done reading. Have any thoughts? You can leave a review on the site itself.  </p>
+                        
+                        <a href="projects/computer_viruses/index.php">Computer Viruses</a>
+                        
+                        <p class="subacctext">for ISTE-240 (Web and Mobile II) at RIT</p>
+                        
+                        <p>An overview of Computer Viruses that includes definitions, types of viruses, a detailed timeline by decade, and more. We modeled the aesthetics after old terminals. Green and black, no images, no complex grid layouts, just text. There is a user survey at the end of the site which you can view the stats for live.</p>
+                        
+                        <a href="projects/online_research/index.html">HTML, CSS, the Internet, and the Web</a>
+                        
+                        <p class="subacctext">for ISTE-140 (Web and Mobile I) at RIT</p>
+                        
+                        <p>A brief summary of HTML, CSS, the Interet, the World Wide Web, and more! This project dives into the history and evolution of websites and what we use to make them.</p>
+                    </section>
+
+                    <section class="subcontent">
+                        <h3>Graphic Design</h3>
+                        <a href="https://drive.google.com/file/d/1CDYBJJuj8kxqJeC5LzumVS-eqSRu2Amz/view?usp=sharing">Subway Autobiography</a>
+                        <p class="subacctext">
+                            for NDME-111 (New Media Digital Design Survey 1) at RIT
+                        </p>
+                        <p>
+                            An autobiography that covers events from many different parts of my life, with each aspect being a "station" in a subway and each event being a "stop". I used a playful child-like aesthetic with train tracks acting as the subway lines and building blocks placed throughout that happen to spell my name. This was created in Adobe Illustrator with my own vector graphics.
+                        </p>
+
+                        <a href="https://drive.google.com/file/d/1oQNbsMp_FSWoVhW63bDXtzx5GTvalrXY/view?usp=sharing">How to Personalize an Ita Bag: Step-by-Step</a>
+                        <p class="subacctext">
+                        for NDME-111 (New Media Digital Design Survey 1) at RIT
+                        </p>
+                        <p>
+                            A step-by-step guide on how to personalize an Ita Bag. It has an ocean blue color palette with deep blues and light blues as contrast. Each step has original vector illustrations created in Adobe Illustrator that help the reader understand the steps.
+                        </p>
+
+                        <a href="https://drive.google.com/file/d/1txJdjjgQQzRHVdn6Z1EgkgMFN-N7I-c7/view?usp=sharing">Fringe Street Beat Promotional Posters</a>
+                        <p class="subacctext">
+                            for NDME-111 (New Media Digital Design Survey 1) at RIT
+                        </p>
+                        <p>
+                            I was tasked with creating a poster for an event, Fringe Street Beat, that already happened as an exercise in graphic design using Adobe Illustrator. I had to make a "simple" version where I was constrained to greyscale, limited text size options, and a grid. Then, I turned my "simple" poster into a colorful dynamic poster where I had full creative freedom and I could break rules to aid the design.
+                        </p>
+
+                    </section>
+                </section>
+            </article>
+
+            <article>
+                <div class="cat-title">   
+                    <h2>Work Experience</h2>
+                </div>
+                
+                <section class="subcontent" id="work">
+                <h3>RIT iSchool Department  - Teaching Assistant, Web and Mobile I</h3>
+                <div class="subacctext">Aug. 2026 to Present</div>
+                <p>Provides academic support to students during class times, inspects code, and answers questions.
+                Collaborates with the professor to grade exercises, assignments, projects, and practical exams.</p>
+
+                <h3>RIT Performing Arts Center  - Front of House Staff</h3>
+                <div class="subacctext">Feb. 2026 to Present</div>
+                <p>Supports event operations for shows, plays, and performances by assisting guests with ticket purchases and scanning, coat check, and general inquiries.
+                Monitors facility during rehearsals, conducting routine inspections, and closing up singehandedly.</p>
+
+                <h3>RIT Campus Life  - Orientation Leader</h3>
+                <div class="subacctext">Aug. 2026</div>
+                <p>Led a group of 20+ incoming RIT students through activities, events, and gatherings during Orientation Week.
+                Escorted families during move-in days. Staffed at RIT club events. Answered questions about life on campus.</p>
+
+                <h3>The Coder School - Code Coach</h3>
+                <div class="subacctext">Summer 2024</div>
+                <p>Instructed technology-focused  “coding camps” for children ages 6-12 such as covering various topics such as coding with drones, 3D modeling, and video production.
+                Facilitated learning by providing individual student support, routine workspace cleaning, and repairs.</p>
+
+                </section>
+            </article>
+
+        </main>
+
+        <footer>
+            <h3>Contact Information:</h3>
+            <ul>
+                <li>Business Email: rym36.webp@gmail.com</li>
+                <li>RIT Email: rrm6931@rit.edu
+                <li>GitHub: ryanmurphy36</li>
+                <li>Also uses Slack!</li>
+            </ul>
+        </footer>
+</body>
+
+</html>
